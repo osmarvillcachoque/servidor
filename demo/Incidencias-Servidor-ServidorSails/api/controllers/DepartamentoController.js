2480d91d2e3cc9e53cc01a720efec393f442bfd4
osmarvillcachoque <osmarpractica@gmail.com>
2016-05-27 12:55:44 -0700

    nada

--- demo/Incidencias-Servidor-ServidorSails/api/controllers/DepartamentoController.js
+++ demo/Incidencias-Servidor-ServidorSails/api/controllers/DepartamentoController.js
@@ -1,6 +1,61 @@
 module.exports = {
-	
+
 	find: function (req, res, next){
+
+		Departamento.find().populateAll().then(function(Departamentos){
+
+			var DepartamentosJSON = [];
+			var Subdepartamentos = [];
+			var FindSubdepartamento;
+			var Instalaciones = [];
+			if(Departamentos){
+				Departamentos.forEach(function(Departamento){
+					DepartamentoJSON = {
+						"id":Departamento.id,
+						"Nombre":Departamento.Nombre,
+						"Instalaciones":"",
+					}
+					FindSubdepartamento = Subdepartamento.find({
+							where:{Departamento:Departamento.id}
+						}).populate('Instalaciones').then(function(subdepartamentos){
+							//console.log(subdepartamentos);
+							subdepartamentos.forEach(function(subdepartamento){
+								return ( Subdepartamentos.push(subdepartamentos), Instalaciones.push(subdepartamento.Instalaciones) );
+							});
+							
+						});
+					DepartamentosJSON.push(DepartamentoJSON);
+				});
+
+				return [DepartamentosJSON, FindSubdepartamento, Subdepartamentos, Instalaciones];
+			}
+			
+
+		})
+		.spread(function(DepartamentosJSON, FindSubdepartamento, Subdepartamentos, Instalaciones) {
+
+			/*DepartamentosJSON.forEach(function(DepartamentoJSON, index) {
+				Subdepartamentos.forEach(function(Subdepartamento, index2){
+					DepartamentoJSON[index].Subdepartamento[index2] = Subdepartamento[index];
+					DepartamentoJSON.Departamento = Departamentos[index];
+				})
+				
+			})*/
+			console.log("DepartamentosJSON");
+			console.log(DepartamentosJSON);
+			console.log("FindSubdepartamento");
+			console.log(FindSubdepartamento);
+			console.log("Subdepartamentos");
+			console.log(Subdepartamentos);
+			console.log("Instalaciones");
+			console.log(Instalaciones);
+			return res.json(DepartamentosJSON);
+		})
+
+		.catch(function(error){ next(error); });
+	
+	}
+	/*find: function (req, res, next){
 	
 		Departamento.find().populateAll().then(function(Departamentos){
 	
@@ -13,10 +68,6 @@ module.exports = {
 					 var Instalaciones = [];
 
 					Departamento.Instalaciones.forEach(function(Instalacion){
-						
-						delete Instalacion.createdAt;
-						delete Instalacion.updatedAt;
-						delete Instalacion.Departamento;
 						
 						Instalaciones.push({ id: Instalacion.id, Nombre: Instalacion.Nombre });
 
@@ -35,6 +86,6 @@ module.exports = {
 	
 		}).catch(function(error){next(error);});
 	
-	}
+	}*/
 
 };
--- demo/Incidencias-Servidor-ServidorSails/api/controllers/SubdepartamentoController.js
+++ demo/Incidencias-Servidor-ServidorSails/api/controllers/SubdepartamentoController.js
@@ -1,10 +1,3 @@
-/**
- * SubdepartamentoController
- *
- * @description :: Server-side logic for managing Subdepartamentoes
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
-
 module.exports = {
 	
 };
