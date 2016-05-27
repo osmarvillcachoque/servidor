975d637f2e2f680e3f8dd62f8a13bb4525ba297f
osmarvillcachoque <osmarpractica@gmail.com>
2016-05-27 00:05:47 -0700

    nada

--- demo/Incidencias-Servidor-ServidorSails/api/controllers/InstalacionController.js
+++ demo/Incidencias-Servidor-ServidorSails/api/controllers/InstalacionController.js
@@ -1,10 +1,3 @@
-/**
- * InstalacionController
- *
- * @description :: Server-side logic for managing Instalacions
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
-
 module.exports = {
 	
 };
